7f1359c71dfeca7f71d7e589dd046fb85d83a9dd
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2021-07-24 20:59:31 +0300

    add json datafile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import data from './components/data/link'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React now!
-        </a>
-      </header>
+      {data.map(({ id, text, value }) => (
+        <div className="List" key={id}>{text}</div>
+      ))}
     </div>
   );
 }
